7fe7eb3b6010e6c61021ef1650cedead3886442c
ruirui <yruirui96@gmail.com>
2022-11-11 21:41:20 +0800

    Dochtml页面结构完成

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,12 +1,16 @@
 <template >
     <Topnav />
-    <div class="banner">
-        <h1>咕噜UI</h1>
-        <h2>一个腻害的UI框架</h2>
-        <p class="action">
-            <a>开始</a>
-            <a>github</a>
-        </p>
+    <div class="content">
+        <aside>
+            <h2>组件列表</h2>
+            <ol>
+                <li><router-link to='/doc/switch'>Switch组件</router-link></li>
+                <li><router-link to='/doc/button'>Button组件</router-link></li>
+                <li><router-link to='/doc/dialog'>Dialog组件</router-link></li>
+                <li><router-link to='/doc/tabs'>Tabs组件</router-link></li>
+            </ol>
+        </aside>
+        <main>主内容</main>
     </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,12 @@
 <template >
     <Topnav />
-    <div class="content">
+    <div class="banner">
         <aside>边栏</aside>
         <main>主内容</main>
-        
+        <p class="actions">
+        <a href="https://github.com">GitHub</a>
+        <router-link to="/doc">开始</router-link>
+      </p>
     </div>
 </template>
 
@@ -17,5 +20,25 @@ export default {
 
 
 <style lang="scss" scoped>
-
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions{
+    padding: 8px 0;
+    a {
+      margin:0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
 </style>
