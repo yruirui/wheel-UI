0549334c8940e1e979e9b927d381edde39a84481
ruirui <yruirui96@gmail.com>
2022-11-25 15:34:07 +0800

    创建button组件

---
+++ src/components/ButtonDemo.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>this is button</div>
+    <h1>示例1</h1>
+    <Button> hi</Button>
+</template>
+<script>
+import Button from '../lib/Button.vue'
+
+export default {
+    components:{Button},
+    setup() {
+        
+    },
+}
+</script>
--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import { createWebHashHistory ,createRouter} from 'vue-router'
 import Home from './views/Home.vue'
 import Doc from './views/Doc.vue'
 import SwitchDemo from './components/SwitchDemo.vue'
+import ButtonDemo from './components/ButtonDemo.vue'
 
 const history=createWebHashHistory()
 export const router=createRouter({
@@ -9,7 +10,8 @@ export const router=createRouter({
     routes:[
         {path:'/',component:Home },
         {path:'/doc',component:Doc,children:[
-            {path:'switch',component:SwitchDemo}
+            {path:'switch',component:SwitchDemo},
+            {path:'button',component:ButtonDemo}
         ]}
     ]
 })
