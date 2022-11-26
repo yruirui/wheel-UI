2928c4230a51b465bb0f144ec0b549d63a300651
ruirui <yruirui96@gmail.com>
2022-11-26 16:55:40 +0800

    事件绑定示例

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,7 +1,9 @@
 <template>
     <div>this is button</div>
     <h1>示例1</h1>
-    <Button> hi</Button>
+    <Button @click="onClick"
+    @focus="onClick"
+    @mouseover="onClick"> hi</Button>
 </template>
 <script>
 import Button from '../lib/Button.vue'
@@ -9,7 +11,10 @@ import Button from '../lib/Button.vue'
 export default {
     components:{Button},
     setup() {
-        
+        const onClick=()=>{
+            console.log('hi')
+        }
+        return {onClick}
     },
 }
 </script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,17 @@
 <template>
-    <div>
-       <slot></slot>
+    <div :size="size">
+      <button v-bind="$attrs"><slot></slot></button> 
     </div>
 </template>
+
+<script lang="ts">
+export default{
+    inheritAttrs:false,
+    setup(props,context) {
+        const {size, ...rest}=context.attrs
+        return {
+            size,rest
+        }
+    },
+}
+</script>
