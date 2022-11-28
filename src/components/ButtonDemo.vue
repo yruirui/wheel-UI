d2ca3bb8735fc470f44e9998e556c6fab436a8bd
ruirui <yruirui96@gmail.com>
2022-11-28 16:36:28 +0800

    size的css

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -5,6 +5,20 @@
     <Button theme="button"> hi</Button>
     <Button theme="link"> hi</Button>
     <Button theme="text"> hi</Button>
+    <h1>示例2</h1>
+    <Button size="big"> 大大大</Button>
+    <Button > 普通</Button>
+    <Button size="small" >小小小 </Button>
+    <div>
+    <Button size="big" theme="link"> 大大大</Button>
+    <Button theme="link" > 普通</Button>
+    <Button size="small" theme="link">小小小 </Button>
+    </div>
+    <div>
+    <Button size="big" theme="text"> 大大大</Button>
+    <Button theme="text" > 普通</Button>
+    <Button size="small" theme="text">小小小 </Button>
+    </div>
 </template>
 <script>
 import Button from '../lib/Button.vue'
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,15 +1,21 @@
 <template>  
-      <button class="gulu-button" :class="{[`gulu-theme-${theme}`]:theme}"><slot></slot></button> 
+      <button class="gulu-button" :class="classes"><slot></slot></button> 
 </template>
 
 <script lang="ts">
+import { computed } from 'vue'
 export default{
     props:{
         theme:{type:String,default:'button'},
         size:{type:String,default:'normal'}
     },
-    setup() {
-        
+    setup(props) {
+      const {theme,size} = props
+       const classes = computed(()=>{
+          return {[`gulu-theme-${theme}`]:theme,
+          [`gulu-size-${size}`]:size}
+        })
+        return {classes}
     },
 }
 </script>
@@ -62,6 +68,16 @@ $radius: 4px;
     &:hover,&:focus{
       background: darken(white, 5%);;
     }
+  }
+  &.gulu-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+  &.gulu-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
   }
 }
 
