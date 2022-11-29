f894dea22325767c1eb0802ead6b8519268a0cc3
ruirui <yruirui96@gmail.com>
2022-11-29 21:47:34 +0800

    实现dialog的弹出

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,6 +1,8 @@
 <template>
-    <div class="dialog-overlay"></div>
-    <div class="dialog-wrapper">
+    <template v-if="visible">
+    <div class="gulu-dialog-overlay"></div>
+    <div class="gulu-dialog-wrapper">
+        <div class="gulu-dialog">
         <header>标题</header>
         <main>
             <p>one</p>
@@ -10,13 +12,76 @@
             <Button>ok</Button>
             <Button>Cancel</Button>
         </footer>
+        </div>
     </div>
+  </template>
 </template>
 
 <script lang="ts">
 import Button from '../lib/Button.vue'
 
 export default{
+    props:{
+        visible:{
+            type:Boolean,
+            default:false
+        }
+    },
     components:{ Button}
 }
 </script>
+
+<style lang="scss">
+$radius:4px;
+$border-color:#d9d9d9;
+
+.gulu-dialog {
+  background: white;
+  border-radius: $radius;
+  box-shadow: 0 0 3px fade_out(black, 0.5);
+  min-width: 15em;
+  max-width: 90%;
+  &-overlay{
+    position:fixed;
+    top:0;
+    left:0;
+    width:100%;
+    height:100%;
+    background: fade_out(black,0.4);
+    z-index:10;
+  }
+  &-wrapper{
+    position: fixed;
+    top:50%;
+    left:50%;
+    transform:translate(-50%,-50%);
+    z-index: 11;
+  }
+  
+  >header{
+    padding:12px 16px;
+    border-bottom:1px solid $border-color;
+    display:flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size:20px;
+  }
+ >main{
+    padding:12px 16px;
+ }
+>footer{
+    border-top:1px solid $border-color;
+    padding:12px 16px;
+    text-align:right;
+&-close{
+    position:relative;
+    display: inline-block;
+    width: 16px;
+    height: 16px;
+    cursor:pointer;
+}
+}
+
+}
+
+</style>
