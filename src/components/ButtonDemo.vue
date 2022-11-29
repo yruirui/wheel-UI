2f1c7d6b0af94cb19a761414031081a3ef197dff
ruirui <yruirui96@gmail.com>
2022-11-29 19:27:45 +0800

    完成button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -21,19 +21,27 @@
     </div>
     <div>
         <Button level="main">主要按钮</Button>
-        <button > 普通按钮</button>
-        <button level="danger">危险按钮</button>
+        <Button > 普通按钮</Button>
+        <Button level="danger">危险按钮</Button>
     </div>
+    <h1>示例3</h1>
     <div>
         <Button theme="link" level="main">主要链接按钮</Button>
       <Button theme="link">普通链接按钮</Button>
       <Button theme="link" level="danger">危险链接按钮</Button>
     </div>
-    <div>
-        <Button theme="text" level="main">主要文字按钮</Button>
-      <Button theme="text">普通文字按钮</Button>
-      <Button theme="text" level="danger">危险文字按钮</Button>
-    </div>
+    
+    <h1>示例4</h1>
+  <div>
+    <Button disabled>禁用按钮</Button>
+    <Button disabled theme="link">禁用链接按钮</Button>
+    <Button theme="text" disabled>禁用按钮</Button>
+  </div>
+  <h1>示例5</h1>
+  <div>
+    <Button loading>加载中</Button>
+    <Button> 加载完毕</Button>
+  </div>
 </template>
 <script>
 import Button from '../lib/Button.vue'
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,6 @@
 <template>  
-      <button class="gulu-button" :class="classes"><slot></slot></button> 
+      <button class="gulu-button" :class="classes" :disabled="disabled">
+      <span class="gulu-lodaingIndicator" v-if="loading"></span><slot></slot></button> 
 </template>
 
 <script lang="ts">
@@ -8,10 +9,12 @@ export default{
     props:{
         theme:{type:String,default:'button'},
         size:{type:String,default:'normal'},
-        level:{type:String,default:'normal'}
+        level:{type:String,default:'normal'},
+        disabled:{type:Boolean,default:false},
+        loading:{type:Boolean,default:false}
     },
     setup(props) {
-      const {theme,size,level} = props
+      const {theme,size,level,loading} = props
        const classes = computed(()=>{
           return {[`gulu-theme-${theme}`]:theme,
           [`gulu-size-${size}`]:size,
@@ -29,10 +32,10 @@ $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
 $red:red;
+$grey:grey;
 .gulu-button {
   box-sizing: border-box;
   height: $h;
-
   padding: 0 12px;
   cursor: pointer;
   display: inline-flex;
@@ -45,8 +48,62 @@ $red:red;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
   transition: background 250ms;
+
+  &.gulu-theme-button {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+      &:hover {
+        border-color: $grey;
+      }
+    }
+  }
+  &.gulu-theme-link, &.gulu-theme-text {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+    }
+  }
+
   & + & {
     margin-left: 8px;
+  }
+  &:hover,
+  &:focus {
+    color: $blue;
+    border-color: $blue;
+  }
+  &:focus {
+    outline: none;
+  }
+  &::-moz-focus-inner {
+    border: 0;
+  }
+  &.gulu-theme-link{
+    border-color:transparent;
+    box-shadow: none;
+    color:$blue;
+    &:hover,&:focus{
+        color:lighten($blue,10%)
+    }
+  }
+  &.gulu-theme-text{
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,&:focus{
+      background: darken(white, 5%);;
+    }
+  }
+  &.gulu-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+  &.gulu-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
   }
 
   &.gulu-theme-button {
@@ -96,6 +153,21 @@ $red:red;
       }
     }
   }
+  > .gulu-lodaingIndicator{
+    
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px; 
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: gulu-spin 1s infinite linear;
+   }
 }
-
+@keyframes gulu-spin{
+  0%{transform:rotate(0deg)}
+  100%{transform:rotate(360deg)}
+}
 </style>
