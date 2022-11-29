4e1ab282facb99a989104c4500465994f6b8fe8c
ruirui <yruirui96@gmail.com>
2022-11-29 20:07:53 +0800

    创建dialog组件

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,3 +1,22 @@
 <template>
-    <div>this is dialog</div>
+    <div class="dialog-overlay"></div>
+    <div class="dialog-wrapper">
+        <header>标题</header>
+        <main>
+            <p>one</p>
+            <p>two</p>
+        </main>
+        <footer>
+            <Button>ok</Button>
+            <Button>Cancel</Button>
+        </footer>
+    </div>
 </template>
+
+<script lang="ts">
+import Button from '../lib/Button.vue'
+
+export default{
+    components:{ Button}
+}
+</script>
